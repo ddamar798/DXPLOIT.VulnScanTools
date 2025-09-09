f602b512aff0ec27e31955ca90dbd250b588fbcf
ddamar798 <damarbudiprasetyo714@gmail.com>
2025-09-09 21:51:02 +0700

    repair folder and file location | change struktur folder for optimation tools

--- dxploit.py
+++ dxploit.py
@@ -1,4 +1,4 @@
-from dxploit.plugins import cli
+from dxploit_vulnScaan.plugins import cli
 
 if __name__ == "__main__":
     cli.run_cli()
--- dxploit/aggregator.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-
-def aggregate_findings(findings: list):
-    severity_count = Counter([f["severity"] for f in findings])
-    return {
-        "summary": dict(severity_count),
-        "findings": findings
-    }
